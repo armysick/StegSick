18c2199bbea4e5f298631683b6b52946315c0464
armysick <armysick@github.com>
2016-08-16 08:19:27 +0100

    Stegifying successfuly on bmp

--- stegifier.c
+++ stegifier.c
@@ -4,19 +4,31 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]){
-	int DEBUG = 1;
+	int DEBUG = 0;
 	FILE *fd;
+	FILE *target;
     unsigned char c;
 	
     
-    fd = fopen("test.txt", "r");
+    fd = fopen(argv[1], "r");
 	if(fd == NULL){
 		printf("error opening file.");
 		return -1;
 	}
+	
+	target = fopen("stegified.bmp", "w");
+	if(target == NULL){
+		printf("error creating file.");
+		return -1;
+	}
+	
 	int curr_to_encryp_bit = 1;
 	int first;
+	int counter = 0;
 	while((first = fgetc(fd)) != EOF){
+		
+		counter++;
+		if(counter < 0){
 		if(DEBUG)
 		printf("READ      : <0x%x>\n", first);
 		if(curr_to_encryp_bit == 0){
@@ -27,9 +39,15 @@ int main(int argc, char *argv[]){
 		}
 		if(DEBUG)
 		printf("CHANGED TO: <0x%x>\n\n", first);
+		
+		}
+		fputc(first, target);
+	
+		
 	}
     fclose(fd);
-
+	fclose(target);
+	printf("\n%i bytes read!\n", counter);
     
     return 0;
 }
